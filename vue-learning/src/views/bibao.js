c32e3fefce68e5d7e6ea31d60cb171e914c8a965
lizhixiang <2528212410@qq.com>
2019-12-18 20:09:58 +0800

    extend bibao

---
+++ vue-learning/src/views/bibao.js
@@ -0,0 +1,28 @@
+for (var let = 0; i < 5; i++){
+	setTimeout(function(){
+		console.log(i);
+	}, 1000)
+}
+// 使用let
+for (let i = 0; i < 5; i++){
+	setTimeout(function(){
+		console.log(i);
+	}, 1000)
+}
+//使用闭包
+for (var i = 0; i < 5; i++){
+	(function(j){
+		setTimeout(function(){
+			console.log(j);
+		}, 1000)
+	})(i)
+}
+//值传递
+for (var i = 0; i < 5; i++){
+	print(i);
+}
+function print(i) {
+	setTimeout(function(){
+		console.log(i)
+	}, 1000)
+}
---
+++ vue-learning/src/views/extend.js
@@ -0,0 +1,68 @@
+class Person {
+	constructor(option) {
+		this.init(option)
+	}
+	init(option) {
+		this.name = option.name;
+		this.age = option.age;
+		this.say();
+	}
+	say() {
+		console.log(this.name, this.age)
+	}
+}
+class People extends Person {
+	constructor(option) {
+		super(option);
+		this.sayHello();
+	}
+	sayHello() {
+		console.log('hello')
+	}
+}
+let aa = new People({name: 'lizhixiang', age: 18});
+function Person(option) {
+	this.name = option && option.name;
+	this.age = option && option.age;
+	this.sleep = function() {
+		console.log(this.name + '正在睡觉');
+	}
+}
+Person.prototype = {
+	constructor: Person,
+	say: function() {
+		console.log(this.name, this.age)
+	}
+}
+
+function People(option) {
+	console.log(111)
+	this.name = option.name;
+	this.age = option.age;
+}
+// 原型继承
+// People.prototype = Person.prototype;
+// Person.prototype.constructor = People;
+// 原型链继承 子类的prototype=父类的实例
+People.prototype = new Person();
+let aa = new People({name: 'lizhixiang', age: 18});
+
+function People(option) {
+	this.name = option.name;
+	this.age = option.age;
+	this.friends = ['xiaoming', 'xiaohong'];
+}
+People.prototype.say = function(){
+	console.log('我的名字是：', this.name);
+}
+
+function Person(option){
+	console.log('option', option)
+	People.call(this, option);
+}
+Person.prototype = new People();
+Person.prototype.constructor = People;
+Person.prototype.sayHello = function() {
+	console.log(this.age);
+}
+var aa = new Person({name: 'lizhixiang', age: '24'});
